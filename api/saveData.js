908c8ec482818be3048aa00908bbdb929cb65980
mardIn smidty <147711727+Martin336699@users.noreply.github.com>
2024-12-11 11:57:04 +0100

    .

--- api/saveData.js
+++ api/saveData.js
@@ -3,14 +3,14 @@ const path = require('path');
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    const newData = req.body;
+    const data = JSON.stringify(req.body, null, 2);
+    const filePath = path.join(process.cwd(), 'api', 'data.json');
 
-    fs.writeFile(path.join(process.cwd(), 'api', 'data.json'), JSON.stringify(newData, null, 2), (err) => {
+    fs.writeFile(filePath, data, (err) => {
       if (err) {
-        console.error('Error writing to file', err);
-        res.status(500).send('Error saving data');
+        res.status(500).send('Fehler beim Speichern der Datei');
       } else {
-        res.status(200).send('Data saved successfully');
+        res.status(200).send('Daten erfolgreich gespeichert');
       }
     });
   } else {

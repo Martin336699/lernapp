9d87b9ecbb5e06e9858c0a074039f6ff7e8785d4
mardIn smidty <147711727+Martin336699@users.noreply.github.com>
2025-02-07 12:05:06 +0100

    login finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,148 +1,61 @@
-// Import necessary modules from react-router-dom for routing
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState, useRef, useEffect } from 'react';
-// Import the CSS file for styling
+import { useState, useEffect } from 'react';
 import '../css/app.css';
-// Import the components that will be used in the routes
+import Login from './login.jsx';
 import RenderKarteikarte from './RenderKarteikarte.jsx';
 import InputKarteiKarte from './InputKarteikarte.jsx';
 import RenderKarteikarteCategory from './RenderKarteikarteCategory.jsx';
 import Navigation from './Navigation.jsx';
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { app } from '../firebase.js';
 
-// Import the initialized Firebase app instance
-import { app } from '../firebase.js'; 
-import { getAuth, 
-         createUserWithEmailAndPassword, 
-         setPersistence, 
-         signInWithEmailAndPassword, 
-         browserLocalPersistence,
-         onAuthStateChanged,
-         signOut } from "firebase/auth";
-
-
-// Define the main App component
 function App() {
- 
-// Define the state variable for the user
-const [user, setUser] = useState("");
-const emailRef = useRef();
-const passwordRef = useRef();
+  const [user, setUser] = useState(null);
 
-useEffect(() => {
-  
-  const auth = getAuth(app);
-  onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log(user);
-    setUser(user.email);
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/auth.user
-    const uid = user.uid;
-    // ...
-  } else {
-    // User is signed out
-    // ...
-  }
-});
-}, []);  
-
-const signUp = (event) => {
-
-  event.preventDefault();
-
-  const auth = getAuth(app);
-  createUserWithEmailAndPassword(
-    auth,
-    emailRef.current.value, 
-    passwordRef.current.value)
-    .then((userCredential) => {
-      // Signed up 
-      const user = userCredential.user;
-      console.log(user);
-      // ...
-    })
-    .catch((error) => {
-      console.log(error);
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // ..
+  useEffect(() => {
+    const auth = getAuth(app);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
     });
-}
 
+    return () => unsubscribe();
+  }, []);
 
-const login = (event) => {
-
-  event.preventDefault(); 
-
-  const auth = getAuth(app);
-  setPersistence(auth, browserLocalPersistence)
-  .then(() => {
-    // Existing and future Auth states are now persisted in the current
-    // session only. Closing the window would clear any existing state even
-    // if a user forgets to sign out.
-    // ...
-    // New sign-in will be persisted with session persistence.
-    return signInWithEmailAndPassword(
-      auth, 
-      emailRef.current.value, 
-      passwordRef.current.value);
-  })
-  .catch((error) => {
-    console.log(error);
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-
-}
-
-const logout = () => {
-
-const auth = getAuth(app);
-signOut(auth).then(() => {
-  // Sign-out successful.
-  setUser("");
-}).catch((error) => {
-  // An error happened.
-});
-
-}  
+  const logout = () => {
+    const auth = getAuth(app);
+    signOut(auth).then(() => {
+      setUser(null);
+    }).catch((error) => {
+      console.error('Error signing out:', error);
+    });
+  };
 
   return (
     <>
-      <form>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="email" placeholder="Enter Email..." ref={emailRef}/>
-        <label htmlFor="password">Password</label>
-        <input id="password" type="password" placeholder="Enter Password..." ref={passwordRef}/>
-        <button type="submit" onClick={signUp}>Registrierung</button>
-        <button type="submit" onClick={login}>Login</button>
-      </form>
-      <div>
-        {user === "" ? <p>Kein User</p> : <p>Eingeloggt als: {user}</p>}
-        <button onClick={logout}>Logout</button>
-      </div>
-
-
-      {/* Set up the router for the application */}
-      <BrowserRouter>
-        <div id="app">
-          {/* Include the Navigation component */}
-          <Navigation />
-          {/* Define the routes for the application */}
-          <Routes>
-            {/* Route for rendering categories */}
-            <Route exact path="/renderCat" element={<RenderKarteikarteCategory />} />
-            {/* Route for rendering all items */}
-            <Route exact path="/renderAll" element={<RenderKarteikarte />} />
-            {/* Default route for inputting a new item */}
-            <Route exact path="/" element={<InputKarteiKarte />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      {user ? (
+        <BrowserRouter>
+          <div id="app">
+            <Navigation />
+            <Routes>
+              <Route exact path="/renderCat" element={<RenderKarteikarteCategory />} />
+              <Route exact path="/renderAll" element={<RenderKarteikarte />} />
+              <Route exact path="/" element={<InputKarteiKarte />} />
+            </Routes>
+            <div>
+              <p>Eingeloggt als: {user.email}</p>
+              <button onClick={logout}>Logout</button>
+            </div>
+          </div>
+        </BrowserRouter>
+      ) : (
+        <Login />
+      )}
     </>
   );
 }
 
-// Export the App component as the default export
 export default App;
---
+++ src/components/login.jsx
@@ -0,0 +1,131 @@
+import { useState, useRef, useEffect } from 'react';
+
+import '../css/login.css';
+
+// Import the initialized Firebase app instance
+import { app, database } from '../firebase.js'; 
+import { ref, push } from 'firebase/database'; // Importing Firebase database functions
+import { getAuth, 
+         createUserWithEmailAndPassword, 
+         setPersistence, 
+         signInWithEmailAndPassword, 
+         browserLocalPersistence,
+         onAuthStateChanged,
+         signOut } from "firebase/auth";
+
+
+         function Login() {
+ 
+          // Define the state variable for the user
+          const [user, setUser] = useState("");
+          const [uid, setUid] = useState("");
+          const emailRef = useRef();
+          const passwordRef = useRef();
+          
+          useEffect(() => {
+            const auth = getAuth(app);
+            
+          
+            onAuthStateChanged(auth, (user) => {
+              if (user) {
+                console.log(user.uid);
+                setUser(user.email);
+                // User is signed in, see docs for a list of available properties
+                // https://firebase.google.com/docs/reference/js/auth.user
+                const uid = user.uid;
+          
+              } else {
+                // User is signed out
+              }
+            });
+          }, []);
+          
+          const signUp = (event) => {
+          
+            event.preventDefault();
+          
+            const auth = getAuth(app);
+            createUserWithEmailAndPassword(
+              auth,
+              emailRef.current.value, 
+              passwordRef.current.value)
+              .then((userCredential) => {
+                // Signed up 
+                const user = userCredential.user;
+                console.log(user);
+                setUid(user.uid);
+                // ...
+              })
+              .catch((error) => {
+                console.log(error);
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+              });
+  
+          }
+          
+          
+          const login = (event) => {
+          
+            event.preventDefault(); 
+          
+            const auth = getAuth(app);
+            setPersistence(auth, browserLocalPersistence)
+            .then(() => {
+              // Existing and future Auth states are now persisted in the current
+              // session only. Closing the window would clear any existing state even
+              // if a user forgets to sign out.
+              // ...
+              // New sign-in will be persisted with session persistence.
+              return signInWithEmailAndPassword(
+                auth, 
+                emailRef.current.value, 
+                passwordRef.current.value);
+            })
+            .catch((error) => {
+              console.log(error);
+              // Handle Errors here.
+              const errorCode = error.code;
+              const errorMessage = error.message;
+            });
+          
+          }
+          
+          const logout = () => {
+          
+          const auth = getAuth(app);
+          signOut(auth).then(() => {
+            // Sign-out successful.
+            setUser("");
+          }).catch((error) => {
+            // An error happened.
+          });
+          
+          }  
+          
+            return (
+              <>
+                <div className='login'>
+                <form>
+                  <label htmlFor="email">Email</label>
+                  <input id="email" type="email" placeholder="Enter Email..." ref={emailRef}/>
+                  <label htmlFor="password">Password</label>
+                  <input id="password" type="password" placeholder="Enter Password..." ref={passwordRef}/>
+                  <button type="submit" onClick={signUp}>Registrierung</button>
+                  <button type="submit" onClick={login}>Login</button>
+                </form>
+                </div>
+                <div className='user'>
+                  {user === "" ? <p>Kein User</p> : <p>Eingeloggt als: {user}</p>}
+                  <button onClick={logout}>Logout</button>
+                </div>
+                
+          
+
+              </>
+            );
+          }
+          
+          // Export the App component as the default export
+          export default Login;
